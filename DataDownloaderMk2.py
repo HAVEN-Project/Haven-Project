ef18233fc290c1764dae1da2a1c1b8320f70b7ca
Nicholastorres-1472 <nicholastorres2123@gmail.com>
2025-12-19 13:08:45 -0500

    Fully finished downloader. Check for missing days of data is missing.

--- DataDownloaderMk2.py
+++ DataDownloaderMk2.py
@@ -1,11 +1,13 @@
 import yfinance as yf
 import pandas as pd
 import sqlite3
+import datetime
 
 # Params
+today = datetime.date.today()
 required_cols = ['symbol', 'date', 'open', 'high', 'low', 'close', 'volume']
-tickers = ["NVDA", "AAPL", "COIN"]
-# tickers = ["NVDA","AAPL","GOOG","GOOGL","MSFT","AMZN","AVGO","META","TSLA","WMT","LLY","JPM","V","ORCL","MA","JNJ","XOM","PLTR","NFLX","BAC","ABBV","COST","HD","AMD","PG","GE","UNH","CSCO","KO","CVX","WFC","IBM","MS","CAT","GS","MU","AXP","MRK","CRM","RTX","PM","APP","MCD","TMUS","ABT","TMO","PEP","AMAT","LRCX","C","DIS","LIN","ISRG","QCOM","INTU","BX","GEV","INTC","NOW","BLK","UBER","T","TJX","VZ","SCHW","AMGN","BKNG","NEE","ACN","TXN","DHR","APH","ANET","KLAC","BA","SPGI","COF","GILD","ADBE","PFE","UNP","LOW","PGR","BSX","ADI","SYK","PANW","DE","ETN","WELL","MDT","CRWD","PLD","HON","KKR","CB","COP","VRTX","PH","LMT","HCA","CEG","ADP","HOOD","NEM","BMY","CVS","MCK","NKE","CMCSA","CME","MO","DASH","SBUX","ICE","SO","MMC","GD","MMM","DUK","CDNS","WM","MCO","TT","SNPS","DELL","APO","UPS","AMT","USB","PNC","BK","NOC","SHW","MAR","ELV","HWM","ORLY","ABNB","EMR","REGN","RCL","AON","GM","GLW","CTAS","ITW","ECL","WBD","EQIX","CI","WMB","TDG","MNST","COIN","CMI","JCI","MDLZ","CSX","SPG","FCX","TEL","COR","FDX","NSC","RSG","AJG","PWR","HLT","TFC","TRV","CL","ADSK","STX","FTNT","AEP","MSI","WDC","KMI","WDAY","SLB","ROST","EOG","PCAR","SRE","PYPL","AFL","VST","NXPI","BDX","AZO","PSX","ARES","IDXX","MPC","F","ALL","MET","APD","DLR","LHX","NDAQ","O","ZTS","URI","VLO","DDOG","EA","D","GWW","PSA","FAST","EW","ROP","CMG","CAH","CBRE","AME","OKE","BKR","AMP","AIG","DHI","ROK","MPWR","DAL","FANG","CTVA","TTWO","CARR","AXON","XEL","LVS","EXC","TGT","FICO","YUM","ETR","PAYX","MSCI","PRU","TKO","CTSH","OXY","KDP","GRMN","KR","CCI","A","XYZ","PEG","TRGP","VMC","EBAY","GEHC","MLM","IQV","NUE","HIG","EL","CPRT","FISV","HSY","VTR","RMD","WAB","MCHP","CCL","KEYS","STT","SYY","ED","FIS","EQT","UAL","OTIS","KMB","ACGL","WEC","EXPE","XYL","ODFL","PCG","LYV","KVUE","IR","HUM","RJF","FITB","FOXA","HPE","MTB","WTW","NRG","VICI","SYF","TER","VRSK","SNDK","CHTR","EXR","FOX","LEN","DG","KHC","CSGP","ROL","ADM","IBKR","MTD","HBAN","EME","BRO","TSCO","FSLR","DOV","ATO","EFX","DTE","EXE","BR","ULTA","CBOE","WRB","AEE","NTRS","DXCM","CINF","DLTR","AWK","STZ","FE","ES","BIIB","OMC","TPR","PPL","STLD","CFG","AVB","GIS","STE","CNP","PHM","IRM","VLTO","TDY","LDOS","RF","HAL","LULU","HUBB","EQR","JBL","DVN","PPG","WAT","NTAP","TROW","HPQ","KEY","RL","EIX","VRSN","WSM","ON","CPAY","LH","L","NVR","LUV","CMS","DRI","TSN","PTC","PODD","SBAC","IP","EXPD","CHD","DGX","CNC","CTRA","NI","PFG","TYL","GPN","SW","SMCI","TPL","WST","TRMB","AMCR","JBHT","CDW","INCY","CHRW","PKG","GPC","SNA","ZBH","BG","MKC","LII","TTD","FTV","PNR","ESS","DD","GEN","DOW","APTV","EVRG","GDDY","WY","IT","LNT","HOLX","Q","INVH","IFF","J","COO","MAA","ALB","BBY","TXT","NWS","FFIV","PSKY","ERIE","DECK","NWSA","DPZ","SOLV","LYB","AVY","UHS","ALLE","EG","KIM","BALL","ZBRA","JKHY","VTRS","IEX","MAS","HRL","NDSN","UDR","HII","HST","WYNN","CLX","BXP","REG","AKAM","CF","BEN","BLDR","ALGN","DOC","SWK","IVZ","EPAM","MRNA","AIZ","HAS","RVTY","CPT","GL","DAY","FDS","SJM","PNW","MGM","SWKS","AES","BAX","AOS","CRL","NCLH","GNRC","TAP","APA","PAYC","TECH","HSIC","POOL","MOH","FRT","CPB","DVA","CAG","MOS","LW","ARE","SOLS","LKQ","MTCH","MHK"]
+tickers = ["NVDA", "AAPL", "COIN","SHOP"]
+tickers = ["NVDA","AAPL","GOOG","GOOGL","MSFT","AMZN","AVGO","META","TSLA","WMT","LLY","JPM","V","ORCL","MA","JNJ","XOM","PLTR","NFLX","BAC","ABBV","COST","HD","AMD","PG","GE","UNH","CSCO","KO","CVX","WFC","IBM","MS","CAT","GS","MU","AXP","MRK","CRM","RTX","PM","APP","MCD","TMUS","ABT","TMO","PEP","AMAT","LRCX","C","DIS","LIN","ISRG","QCOM","INTU","BX","GEV","INTC","NOW","BLK","UBER","T","TJX","VZ","SCHW","AMGN","BKNG","NEE","ACN","TXN","DHR","APH","ANET","KLAC","BA","SPGI","COF","GILD","ADBE","PFE","UNP","LOW","PGR","BSX","ADI","SYK","PANW","DE","ETN","WELL","MDT","CRWD","PLD","HON","KKR","CB","COP","VRTX","PH","LMT","HCA","CEG","ADP","HOOD","NEM","BMY","CVS","MCK","NKE","CMCSA","CME","MO","DASH","SBUX","ICE","SO","MMC","GD","MMM","DUK","CDNS","WM","MCO","TT","SNPS","DELL","APO","UPS","AMT","USB","PNC","BK","NOC","SHW","MAR","ELV","HWM","ORLY","ABNB","EMR","REGN","RCL","AON","GM","GLW","CTAS","ITW","ECL","WBD","EQIX","CI","WMB","TDG","MNST","COIN","CMI","JCI","MDLZ","CSX","SPG","FCX","TEL","COR","FDX","NSC","RSG","AJG","PWR","HLT","TFC","TRV","CL","ADSK","STX","FTNT","AEP","MSI","WDC","KMI","WDAY","SLB","ROST","EOG","PCAR","SRE","PYPL","AFL","VST","NXPI","BDX","AZO","PSX","ARES","IDXX","MPC","F","ALL","MET","APD","DLR","LHX","NDAQ","O","ZTS","URI","VLO","DDOG","EA","D","GWW","PSA","FAST","EW","ROP","CMG","CAH","CBRE","AME","OKE","BKR","AMP","AIG","DHI","ROK","MPWR","DAL","FANG","CTVA","TTWO","CARR","AXON","XEL","LVS","EXC","TGT","FICO","YUM","ETR","PAYX","MSCI","PRU","TKO","CTSH","OXY","KDP","GRMN","KR","CCI","A","XYZ","PEG","TRGP","VMC","EBAY","GEHC","MLM","IQV","NUE","HIG","EL","CPRT","FISV","HSY","VTR","RMD","WAB","MCHP","CCL","KEYS","STT","SYY","ED","FIS","EQT","UAL","OTIS","KMB","ACGL","WEC","EXPE","XYL","ODFL","PCG","LYV","KVUE","IR","HUM","RJF","FITB","FOXA","HPE","MTB","WTW","NRG","VICI","SYF","TER","VRSK","SNDK","CHTR","EXR","FOX","LEN","DG","KHC","CSGP","ROL","ADM","IBKR","MTD","HBAN","EME","BRO","TSCO","FSLR","DOV","ATO","EFX","DTE","EXE","BR","ULTA","CBOE","WRB","AEE","NTRS","DXCM","CINF","DLTR","AWK","STZ","FE","ES","BIIB","OMC","TPR","PPL","STLD","CFG","AVB","GIS","STE","CNP","PHM","IRM","VLTO","TDY","LDOS","RF","HAL","LULU","HUBB","EQR","JBL","DVN","PPG","WAT","NTAP","TROW","HPQ","KEY","RL","EIX","VRSN","WSM","ON","CPAY","LH","L","NVR","LUV","CMS","DRI","TSN","PTC","PODD","SBAC","IP","EXPD","CHD","DGX","CNC","CTRA","NI","PFG","TYL","GPN","SW","SMCI","TPL","WST","TRMB","AMCR","JBHT","CDW","INCY","CHRW","PKG","GPC","SNA","ZBH","BG","MKC","LII","TTD","FTV","PNR","ESS","DD","GEN","DOW","APTV","EVRG","GDDY","WY","IT","LNT","HOLX","Q","INVH","IFF","J","COO","MAA","ALB","BBY","TXT","NWS","FFIV","PSKY","ERIE","DECK","NWSA","DPZ","SOLV","LYB","AVY","UHS","ALLE","EG","KIM","BALL","ZBRA","JKHY","VTRS","IEX","MAS","HRL","NDSN","UDR","HII","HST","WYNN","CLX","BXP","REG","AKAM","CF","BEN","BLDR","ALGN","DOC","SWK","IVZ","EPAM","MRNA","AIZ","HAS","RVTY","CPT","GL","DAY","FDS","SJM","PNW","MGM","SWKS","AES","BAX","AOS","CRL","NCLH","GNRC","TAP","APA","PAYC","TECH","HSIC","POOL","MOH","FRT","CPB","DVA","CAG","MOS","LW","ARE","SOLS","LKQ","MTCH","MHK"]
 start_date = "2023-10-01"
 end_date = "2025-10-17"
 download_interval = "1d" # hourly (1h) daily is (1d)
@@ -18,28 +20,32 @@ if (download_interval == "1d"):
 
 data = None 
 
-data = yf.download(tickers, period = "max", group_by='ticker', interval=download_interval)
+# data = yf.download(tickers, period = "max", group_by='ticker', interval=download_interval)
 
 # print(data)
 
 conn = sqlite3.connect("StockData.db")
 cursor = conn.cursor()
 
-    # Example: get the last date we have for a stock
-    # cursor.execute("SELECT MAX(date) FROM DailyData WHERE symbol=?", (tickers,))
-    # last_date = cursor.fetchone()[0]  # this returns 'YYYY-MM-DD' or None if no data
+ticker_dfs = []
 
-if data is not None and not data.empty:
+for ticker in tickers:
+    
+    cursor.execute("SELECT MAX(date) FROM DailyData WHERE symbol=?", (ticker,))
+    last_date = cursor.fetchone()[0]  # this returns 'YYYY-MM-DD' or None if no data
+    if(last_date == None):
+        last_date = "2020-01-01"
+    if(last_date == today):
+        print("Data For Stock already Acquired for today")
+        continue
+    
+    print(last_date)
 
-    ticker_dfs = []
 
-    for ticker in tickers:
-        
-        cursor.execute("SELECT MAX(date) FROM DailyData WHERE symbol=?", (ticker,))
-        last_date = cursor.fetchone()[0]  # this returns 'YYYY-MM-DD' or None if no data
 
-        print(last_date)
+    data = yf.download(ticker,start=last_date, group_by='ticker', interval=download_interval)
 
+    if data:
         #extracts data for the ticker
         ticker_data = data[ticker].copy()
 
@@ -52,25 +58,36 @@ if data is not None and not data.empty:
         #rename columns to match required columns format
         ticker_data.columns = ticker_data.columns.str.lower()
 
+        # 1) If any NaN in required columns -> skip whole ticker
+        if ticker_data[required_cols].isna().any().any():
+            print(f"Skipping {ticker}: missing OHLCV data")
+            continue
 
-        
-        ticker_data = ticker_data.dropna(subset=['date', 'open', 'high', 'low', 'close', 'volume'])
+        # 2) If you also want to enforce *no date gaps* for daily data
+        ticker_data = ticker_data.sort_values('date')
+        ticker_data['date'] = pd.to_datetime(ticker_data['date'])
 
-        if last_date:
-            ticker_data = ticker_data[ticker_data['date'] > last_date]
-        
+        # Build expected calendar from first to last date (business days)
+        expected = pd.date_range(
+            start=ticker_data['date'].iloc[0],
+            end=ticker_data['date'].iloc[-1],
+            freq='B'
+        )
+
+        if len(expected) != len(ticker_data) or not (ticker_data['date'] == expected).all():
+            print(f"Skipping {ticker}: missing trading days")
+            continue
+
+        # Used to only inster data newer then this date
+        cut_off = last_date
+
+        # If passed all checks, keep only the required columns and write
         available_cols = [col for col in required_cols if col in ticker_data.columns]
-
         ticker_data = ticker_data[available_cols]
-
-        ticker_data['date'] = pd.to_datetime(ticker_data['date']).dt.strftime('%Y-%m-%d %H:%M:%S')
-
-        # print(ticker_data.columns)
-
-        # print(ticker_data)
+        ticker_data['date'] = ticker_data['date'].dt.strftime('%Y-%m-%d %H:%M:%S')
+        ticker_data = ticker_data[ticker_data['date']] >= cut_off
 
         ticker_data.to_sql(TABLE_NAME, conn, if_exists='append', index=False)
-
-        #   ticker_dfs.append(ticker_data)
-
-    # ticker_dfs.to_sql(TABLE_NAME, conn, if_exists='append', index=False)
+        
+        
+        
---
+++ DataDownloaderMk3(Daily).py
@@ -0,0 +1,181 @@
+import yfinance as yf
+import pandas as pd
+import sqlite3
+from datetime import datetime, date, timedelta, time
+import logging 
+from logging.handlers import RotatingFileHandler
+
+
+#Logging
+logger = logging.getLogger("Daily_Downloader")
+logger.setLevel(logging.INFO)
+logger.propagate = False
+
+#Formats the logger
+formatter = logging.Formatter(
+    "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+)
+
+file_handler = RotatingFileHandler(
+    "download.log",
+    maxBytes=5*1024*1024,
+    backupCount=100
+)
+
+#Adds handlers
+console_handler = logging.StreamHandler() # Makes output to console
+console_handler.setFormatter(formatter) # Formats the logs
+file_handler.setFormatter(formatter) #formats logs
+logger.addHandler(console_handler) #adds the handler to the logging system (Activate for console logging)
+logger.addHandler(file_handler) #Adds the handler that outputs to logs
+
+
+
+logger.info("Daily download cycle started")
+
+
+# Params
+required_cols = ['symbol_id', 'date', 'open', 'high', 'low', 'close', 'volume']
+error_count = 0
+
+try:
+    # Database configuration
+    conn = sqlite3.connect("TestStockData.db")
+    cursor = conn.cursor()
+    SYMBOL_TABLE = "symbol"
+    DAILY_TABLE = "ohlcv_daily"
+    interval = "1d"
+
+except Exception:
+    logger.exception("Failed Database Connection")
+
+
+#Time Functions
+try:
+    today = date.today()
+    date_format = "%Y-%m-%d"
+    current_time = datetime.now().time()
+    target_time = time(hour=17, minute=30)
+    # today = datetime.strptime(today, "%Y-%m-%d %H-:M:%S")
+except Exception:
+    logger.exception("Failed During Time Functions")
+
+# Get Ticker List Sorts through for only Enabled Stocks
+try:
+    tickers = []
+    cursor.execute("SELECT ticker FROM symbol WHERE enabled=1 AND missing_data=0" )
+    tickers = cursor.fetchall()
+    total_tickers = len(tickers)
+    if tickers == None:
+        raise Exception
+except Exception:
+    logger.exception("Failed to acquire enabled stocks from Database")
+
+# Gets Row Id
+def get_symbol_id(conn, ticker):
+    cur = conn.cursor()
+    cur.execute("SELECT id FROM symbol WHERE ticker = ?", (ticker))
+    row = cur.fetchone()
+    if row:
+        return row[0]
+    # Insert if not exists
+    cur.execute("INSERT INTO symbol (ticker) VALUES (?)", (ticker,))
+    conn.commit()
+    return cur.lastrowid
+
+for ticker in tickers:
+    logger.debug(f"Ticker: {ticker}")
+    try:
+        id = get_symbol_id(conn, ticker)
+        logger.debug(f"Stock ID is: {id}")
+        if id == None:
+            raise Exception
+    except Exception:
+        logger.exception("Failed to acquire Ticker ID")
+        error_count += 1
+        continue
+
+    try:
+        cursor.execute(f"SELECT MAX(date) FROM {DAILY_TABLE} WHERE symbol_id=?", (id,))
+        last_date = cursor.fetchone()[0]  # this returns 'YYYY-MM-DD' or None if no data
+    except Exception:
+        logger.exception(f"Failed to acquire last date of {ticker}")
+        error_count += 1
+        continue
+    # lastdate = datetime.date(last_da)
+    # today = datetime.strptime(today, date_format).date()
+    # if (DEBUG == True):
+    #     print(f"Last date: {last_date}")
+    #     print(f"Today: {today}")
+    #     print (f"Current Time: {current_time}")
+    #     print(f"Target Time: {target_time}")
+
+    try:
+        if(last_date == None):
+            last_date = "2020-01-01"
+        last_date = datetime.strptime(last_date, date_format).date()
+        if isinstance(today, str):
+            today = datetime.strptime(today, date_format).date()
+        if(last_date == today or (current_time < target_time and (today - timedelta(days=1) == last_date))):
+            # print(f"Data For {ticker} already Acquired for today. ID: {id}")
+            logger.error(f"Data For {ticker} already Acquired for today. ID: {id}")
+            error_count += 1
+            continue
+        next_date = last_date + timedelta(days=1)
+    except Exception:
+        logger.exception("Failed Time Comparision block")
+        error_count += 1
+        continue
+
+    try:
+        data = yf.download(ticker,start=next_date, group_by='ticker', interval="1d")
+        if data is  None or data.empty:
+            raise Exception
+            
+    except Exception:
+        logger.exception("Failed to download data through Yfinance")
+        error_count += 1
+        continue
+
+    if data is not None and not data.empty:
+        #extracts data for the ticker
+        ticker_data = data[ticker].copy()
+
+        # Add symbol to column
+        ticker_data['symbol_id'] = id
+        
+        # Reset index to make date a column
+        ticker_data = ticker_data.reset_index()
+
+        #rename columns to match required columns format
+        ticker_data.columns = ticker_data.columns.str.lower()
+
+        # 1) If any NaN in required columns -> skip whole ticker
+        if ticker_data[required_cols].isna().any().any():
+            logger.error(f"Skipping {ticker}: missing OHLCV data")
+            error_count += 1
+            continue
+
+        # 2) If you also want to enforce *no date gaps* for daily data
+        ticker_data = ticker_data.sort_values('date')
+        ticker_data['date'] = pd.to_datetime(ticker_data['date'])
+
+        # Used to only insert data newer then this date
+        cut_off = pd.to_datetime(last_date)
+
+        # If passed all checks, keep only the required columns and write
+        available_cols = [col for col in required_cols if col in ticker_data.columns]
+        ticker_data = ticker_data[available_cols]
+        ticker_data['date'] = pd.to_datetime(ticker_data['date'])
+        ticker_data = ticker_data[ticker_data['date'] >= cut_off]
+        ticker_data['date'] = ticker_data['date'].dt.strftime('%Y-%m-%d')
+
+        try:
+            ticker_data.to_sql(DAILY_TABLE, conn, if_exists='append', index=False)
+        except Exception:
+            logger.exception(f"Failed to insert {ticker} to {DAILY_TABLE} | ID: {id}")
+            error_count += 1
+            continue
+        logger.info(f"Finished acquiring data for {ticker} with ID: {id}")
+
+logger.info(f"Finished Daily download cycle for {total_tickers} stocks. With {error_count} errors.")
